8df0388741e66810f3be65241db5adffeac6e56b
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-10-17 18:52:17 +0000

    [update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'mypage' => 'users#mypage', as: 'mypage'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/unsubscribe'
-  get 'reviews/new'
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/edit'
   devise_for :users
   root to: "homes#top"
+  get 'mypage' => 'users#mypage', as: 'mypage'
+  resources :users, only: [:show, :edit, :unsubscribe]
+  resources :reviews, only: [:new, :index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
