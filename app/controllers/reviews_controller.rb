5274ec25724de84b7fdc5cf038dbcbbcd0e478e5
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-10-30 11:01:00 +0000

    [fix]review_etc

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -41,7 +41,7 @@ class ReviewsController < ApplicationController
   def destroy
     review = Review.find(params[:id])
     review.destroy
-    redirect_to '/reviews'
+    redirect_to '/mypage'
   end  
   
   
