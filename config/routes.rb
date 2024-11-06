380139ab67a404be65622b9717fa1623703e3df9
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-11-06 18:53:02 +0000

    [add]admin_authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
   devise_for :users
   root to: "homes#top"
   get 'mypage' => 'users#mypage', as: 'mypage'
